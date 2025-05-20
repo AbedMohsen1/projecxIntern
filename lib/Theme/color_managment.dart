40c94d4e663ca4c32307c58fae8acf88436e600c
AbedMohsen1 <123845315+AbedMohsen1@users.noreply.github.com>
2025-05-20 10:33:39 +0300

    Update color_managment.dart

--- lib/Theme/color_managment.dart
+++ lib/Theme/color_managment.dart
@@ -1,1 +1,11 @@
 
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class AppColors {
+  static Color backTest = HexColor("#29ABE2");
+  static Color black = HexColor("#353D51");
+  static Color blu = HexColor("#29ABE2");
+  static Color blu2 = HexColor("#BCE7F0");
+  static Color white = HexColor("#FFFFFF");
+}
