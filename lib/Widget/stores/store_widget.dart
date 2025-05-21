2ca66e40882583fb82153f797de11d75820a725d
AhdOthman <30952811+AhdOthman@users.noreply.github.com>
2025-05-21 16:51:46 +0300

    update

---
+++ lib/Widget/stores/store_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class StoreWidget extends StatelessWidget {
+  final String imagePath;
+  final Function onTap;
+  const StoreWidget({super.key, required this.imagePath, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
